649e1594005c862d0771074605f083f94c4a6036
jyunee <95419701+jynnnnnn@users.noreply.github.com>
2025-05-02 15:07:54 +0900

    [Refactor/#5] MyNavGraph 수정

--- app/src/main/java/org/sopt/at/navigation/MyNavGraph.kt
+++ app/src/main/java/org/sopt/at/navigation/MyNavGraph.kt
@@ -8,6 +8,6 @@ import org.sopt.at.my.MyScreen
 fun NavGraphBuilder.myNavGraph(navController: NavHostController) {
     composable("MyScreen/{userId}") { backStackEntry ->
         val userId = backStackEntry.arguments?.getString("userId") ?: "프로필"
-        MyScreen(navController, userId)
+        MyScreen(navController)
     }
 }
